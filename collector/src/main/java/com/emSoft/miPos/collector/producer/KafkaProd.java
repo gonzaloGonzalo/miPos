432e4e4768bbf5e7d31a59d3c7d488332d29b7c1
Garay, Gonzalo <Gonzalo_Garay@McAfee.com>
2018-05-09 23:47:06 -0300

    Adding producer modifications.

--- collector/src/main/java/com/emSoft/miPos/collector/producer/KafkaProd.java
+++ collector/src/main/java/com/emSoft/miPos/collector/producer/KafkaProd.java
@@ -1,12 +1,9 @@
 package com.emSoft.miPos.collector.producer;
 
-
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import java.util.Properties;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 public class KafkaProd {
 
@@ -26,39 +23,24 @@ public class KafkaProd {
         return new KafkaProducer<>(props);
     }
 
-    static void runProducer(final int sendMessageCount) throws InterruptedException {
+    static void runProducer(final String message) throws InterruptedException {
         final Producer<Long, String> producer = createProducer();
-        long time = System.currentTimeMillis();
-        final CountDownLatch countDownLatch = new CountDownLatch(sendMessageCount);
         try {
-            for (long index = time; index < time + sendMessageCount; index++) {
-                final ProducerRecord<Long, String> record =
-                        new ProducerRecord<>(TOPIC, index, "Hello Mom " + index);
-                producer.send(record, (metadata, exception) -> {
-                    long elapsedTime = System.currentTimeMillis() - time;
-                    if (metadata != null) {
-                        System.out.printf("sent record(key=%s value=%s) " +
-                                        "meta(partition=%d, offset=%d) time=%d\n",
-                                record.key(), record.value(), metadata.partition(),
-                                metadata.offset(), elapsedTime);
-                    } else {
-                        exception.printStackTrace();
-                    }
-                    countDownLatch.countDown();
-                });
-            }
-            countDownLatch.await(25, TimeUnit.SECONDS);
+            final ProducerRecord<Long, String> record = new ProducerRecord<>(TOPIC,  message);
+            producer.send(record, (metadata, exception) -> {
+                long elapsedTime = System.currentTimeMillis();
+                if (metadata != null) {
+                    System.out.printf("sent record(key=%s value=%s) " +
+                                    "meta(partition=%d, offset=%d) time=%d\n",
+                            record.key(), record.value(), metadata.partition(),
+                            metadata.offset(), elapsedTime);
+                } else {
+                    exception.printStackTrace();
+                }
+            });
         }finally {
             producer.flush();
             producer.close();
-        }
-    }
-
-    public static void main(String... args) throws Exception {
-        if (args.length == 0) {
-            runProducer(5);
-        } else {
-            runProducer(Integer.parseInt(args[0]));
         }
     }
 
